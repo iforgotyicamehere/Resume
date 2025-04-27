da79df67e18c7e3592511d872f9bc3af1c7947b9
Iforgotyicamehere <welovelamp@grasswasabadchoice.com>
2025-04-27 12:23:20 -0600

    added alot of stuff lol

---
+++ public/node.js
@@ -0,0 +1,47 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const nodemailer=require('nodemailer');
+
+const app=express();
+const port=3000;
+
+//middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(express.static('public')); 
+
+//homepage route
+app.get('/', (req, res) => {
+ res.sendFile(__dirname +  /public/index.html);
+});
+//contact from route
+app.post('/contact', (req, res) => {
+    const { name, email, message } = req.body;
+
+    const transporter = nodemailer.createTransport ({
+        service: 'gmail',
+        auth: {
+    user:'daquota92@gmail.com',
+pass: 'Rentsdue1st!'
+    }
+});
+const mailOptions = {
+    from: email,
+    to: 'daquota92@gamil.com',
+    subject: 'I have an idea',
+    text: message
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return res.status(500).send(error.toString());
+    }
+    res.status(200).send('Message Sent; ' + info.response);
+ });
+});
+
+//Start Server
+app.listen(port, () => { 
+    console.log (`Server is running on http://localhost:${port}`)
+});
+
